47e6f0ff689b363f61811a0727244531ef6f99fe
AiresB <baptiste.aires@epitech.eu>
2022-01-29 11:53:55 +0100

    add(main): first beta of the bot to test deployment on Heroku

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+
+from discord.ext import commands
+from dotenv import load_dotenv
+from riot import get_rank, get_list
+
+load_dotenv(dotenv_path="config")
+
+RIOT_TOKEN = os.getenv("RIOT_TOKEN")
+DISCORD_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = commands.Bot(command_prefix="!")
+
+lst = []
+
+@bot.event
+async def on_ready():
+    print("Bot connected")
+
+@bot.command(name="repeat")
+async def repeat(ctx, rep: str):
+    await ctx.message.channel.send(rep)
+
+@bot.command(name="add")
+async def add(ctx, pseudo: str):
+    lst.append(pseudo)
+    await ctx.message.channel.send(pseudo + " added")
+
+@bot.command(name="remove")
+async def rm(ctx, pseudo: str):
+    lst.remove(pseudo)
+    await ctx.message.channel.send(pseudo + " removed")
+
+@bot.command(name="list")
+async def ls(ctx):
+    res = get_list(lst, RIOT_TOKEN)
+    for r in res:
+        await ctx.message.channel.send(r[0])
+
+@bot.command(name="rank")
+async def rank(ctx, pseudo: str, queuetype: str):
+    global RIOT_TOKEN
+    rank, _ = get_rank(pseudo, RIOT_TOKEN, queuetype)
+    await ctx.message.channel.send(rank)
+
+@bot.command(name="token")
+async def token(ctx, token: str):
+    global RIOT_TOKEN
+    RIOT_TOKEN = token
+    async for m in ctx.message.channel.history(limit=1):
+        await m.delete()
+
+bot.run(DISCORD_TOKEN)
