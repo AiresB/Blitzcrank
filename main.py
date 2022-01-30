c401c4eba85d662abaf2a3f1d1e825d07e91d0e2
AiresB <baptiste.aires@epitech.eu>
2022-01-31 00:06:41 +0100

    add(comment): add comment on the code

--- main.py
+++ main.py
@@ -15,26 +15,57 @@ lst = []
 
 @bot.event
 async def on_ready():
+    """
+    type: event
+    activate when the bot is connected and print in the console log
+    """
     print("Bot connected")
 
 @bot.command(name="add")
 async def add(ctx, pseudo: str):
+    """
+    type: command
+    create a new player from the pseudo
+    Args:
+        ctx : [context]
+        pseudo (str): [pseudo of the player created]
+    """
     lst.append(pseudo)
     await ctx.message.channel.send(pseudo + " added")
 
 @bot.command(name="remove")
 async def rm(ctx, pseudo: str):
+    """
+    type: command
+    remove the player from the pseudo
+    Args:
+        ctx : [context]
+        pseudo (str): [pseudo of the player created]
+    """
     lst.remove(pseudo)
     await ctx.message.channel.send(pseudo + " removed")
 
 @bot.command(name="ranking")
-async def ls(ctx):
+async def ranking(ctx):
+    """
+    type: command
+    give the ranking of the players registered
+    Args:
+        ctx : [context]
+    """
     res = get_list(lst, RIOT_TOKEN)
     for r in res:
         await ctx.message.channel.send(r[0])
 
 @bot.command(name="token")
 async def token(ctx, token: str):
+    """
+    type: command
+    change the riot token
+    Args:
+        ctx : [context]
+        token (str): [riot api token]
+    """
     global RIOT_TOKEN
     RIOT_TOKEN = token
     async for m in ctx.message.channel.history(limit=1):
--- riot.py
+++ riot.py
@@ -1,6 +1,14 @@
 import requests
 
 def token_work(token):
+    """[Check the validity of the token]
+
+    Args:
+        token (str): [riot api]
+
+    Returns:
+        [Bool]: [True: token works, False: token doesn't work]
+    """
     p = {"api_key": token}
     res = requests.get("https://euw1.api.riotgames.com/lol/status/v3/shard-data", params=p)
     if res.status_code != 200:
@@ -11,10 +19,26 @@ def token_work(token):
 
 
 def get_print(dic):
+    """[get the ranked string from the dic]
+
+    Args:
+        dic (dictionary): [player dictionnary]
+
+    Returns:
+        [str]: [the rank as string]
+    """
     return f'{dic["summonerName"]}:\t {dic["tier"]}\t {dic["rank"]}\t {dic["leaguePoints"]} LP\t'
 
 
 def get_lp(dic):
+    """[get the rank as lp from 0 to ~3000]
+
+    Args:
+        dic (dictionary): [player dictionnary]
+
+    Returns:
+        [int]: [the rank as lp]
+    """
     tier = {"IRON": 0, "BRONZE": 400, "SILVER": 800, "GOLD": 1200, "PLATINUM": 1600, "DIAMOND": 2000, "MASTER": 2400, "GRANDMASTER": 2400 ,"CHALLENGER": 2400}
     rank = {"IV": 0, "III": 100, "II": 200, "I": 300}
     lp = tier[dic["tier"]] + rank[dic["rank"]] + int(dic["leaguePoints"])
@@ -22,10 +46,30 @@ def get_lp(dic):
 
 
 def get_player(pseudo, param):
+    """[get informations on the player by his pseudo]
+
+    Args:
+        pseudo ([str]): [pseudo of the player searched]
+        param ([dict]): [param from the requests, containing token]
+
+    Returns:
+        [dict]: [dict of the player informations]
+    """
     return requests.get(f'https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/{pseudo}', params=param).json()
 
 
 def get_rank(pseudo, token, queue_type="RANKED_TFT"):
+    """[get the rank of a player given his pseudo and the queue type]
+
+    Args:
+        pseudo ([str]): [pseudo of the player]
+        token ([str]): [riot api token]
+        queue_type (str): [queue type]. Defaults to "RANKED_TFT".
+
+    Returns:
+        [str]: [rank as str]
+        [int]: [rank as int]
+    """
     if not token_work(token):
         return "Error Token, please reload the token with !token [token]", 0
     param = {"api_key": token}
@@ -44,6 +88,16 @@ def get_rank(pseudo, token, queue_type="RANKED_TFT"):
     return f"{pseudo}: Unranked player", 0
 
 def get_list(pseudos, token, queue_type="RANKED_TFT"):
+    """[get the list of the players and return the ranking]
+
+    Args:
+        pseudos ([str]): [pseudos]
+        token ([str]): [riot api token]
+        queue_type (str, optional): [queue type]. Defaults to "RANKED_TFT".
+
+    Returns:
+        [list]: [list of the players sorted by rank]
+    """
     tp = []
     st = []
     for p in pseudos:
