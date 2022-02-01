77f4e083f656f5a09e4d8d71cbed1836b2a6d426
AiresB <baptiste.aires@epitech.eu>
2022-02-01 20:10:11 +0100

    Update base.py

--- src/front/commands/base.py
+++ src/front/commands/base.py
@@ -11,16 +11,16 @@ class Base(discord.ext.commands.Cog, name='Base functions'):
         self.list = []
 
     @discord.ext.commands.command(name="add")
-    async def add(self, ctx, pseudo: str):
+    async def add(self, ctx,*, arg):
         """
         type: command
         create a new player from the pseudo
         Args:
             ctx : [context]
-            pseudo (str): [pseudo of the player created]
+            arg (str): [pseudo of the player created]
         """
-        self.list.append(pseudo)
-        await ctx.message.channel.send(pseudo + " added")
+        self.list.append(arg)
+        await ctx.message.channel.send(arg + " added")
 
     @discord.ext.commands.command(name="remove")
     async def rm(self, ctx, pseudo: str):
